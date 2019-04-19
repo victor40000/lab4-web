8038200fcc2b0323277558d6bffd4761d87ed8cd
victor40000 <victor40000@yandex.ru>
2019-04-19 11:54:48 +0300

    Header added

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Новости</title>
+    <link rel="stylesheet" href="css/custom.css" >
+</head>
+<body>
+<div class="container">
+    <div class="brand"><font color = "#ffffcc">Новости</font></div>
+</div>
+</body>
+</html>
